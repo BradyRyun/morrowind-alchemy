19816122f437dec053a199bbd38aac7ef5ba5390
Brady <bradyryun@gmail.com>
2024-02-11 11:54:58 +0100

    Add copyright and footer

---
+++ src/app/footer.tsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+    return (
+        <div className="min-h-128 w-full">
+            <div className={"font-viner mx-auto text-center w-fit flex flex-col"}>
+                <p>
+                    Unofficial tool created for the love of Morrowind &#x2764;
+                </p>
+                <p>
+                    Copyright &copy; {new Date().getFullYear()} Brady Lee Ryun
+
+                </p>
+            </div>
+        </div>
+
+    )
+}
